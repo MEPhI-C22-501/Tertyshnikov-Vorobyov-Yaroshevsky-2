32e39149e76b8c772cf67367bcfd965be61a8d12
perfecto <67798500+pErfEcto2@users.noreply.github.com>
2024-12-11 19:42:42 +0000

    Add files via upload

--- data_mem.vhd
+++ data_mem.vhd
@@ -22,14 +22,16 @@ end DataMemory;
 
 architecture DataMemory_beh of DataMemory is
     signal memory : memory_array := (others => (others => '0'));
+	signal data1_r : std_logic_vector(31 downto 0) := (others => '0');
+	signal data2_r : std_logic_vector(31 downto 0) := (others => '0');
 
 begin
      
     process (i_clk)
     begin
 	if rising_edge(i_clk) then
-        	o_read_data1 <= memory(to_integer(unsigned(i_read_addr1)));
-		o_read_data2 <= memory(to_integer(unsigned(i_read_addr2)));
+        	o_read_data1 <= data1_r;
+		o_read_data2 <= data2_r;
 	end if;
     end process;
 
@@ -43,10 +45,14 @@ begin
         elsif rising_edge(i_clk) then
             if i_write_enable1  = '1' then
                 memory(to_integer(unsigned(i_write_addr1))) <= i_write_data1;
-            end if;
+            else
+		data1_r <= memory(to_integer(unsigned(i_read_addr1)));
+		end if;
 	
 	    if i_write_enable2  = '1' then
                 memory(to_integer(unsigned(i_write_addr2))) <= i_write_data2;
+	    else
+		data2_r <= memory(to_integer(unsigned(i_read_addr2)));
             end if;
         end if;
     end process;
