884c33b51a9239d9c44d6d99c5ba41c835e1d106
perfecto <67798500+pErfEcto2@users.noreply.github.com>
2024-12-16 11:40:42 +0000

    Add files via upload

--- data_mem_tb.vhd
+++ data_mem_tb.vhd
@@ -10,8 +10,7 @@ architecture data_mem_tb_arch of data_mem_tb is
     	signal clk_r        : std_logic := '0';
     	signal rst_r        : std_logic := '0';
     	signal write_enable_r   : std_logic := '0';
-    	signal write_addr_r : std_logic_vector(15 downto 0) := (others => '0');
-	signal read_addr_r : std_logic_vector(15 downto 0) := (others => '0');
+    	signal addr_r : std_logic_vector(15 downto 0) := (others => '0');
     	signal write_data_r : std_logic_vector(31 downto 0) := (others => '0');
 	signal read_data_r : std_logic_vector(31 downto 0) := (others => '0');
     	
@@ -20,8 +19,7 @@ architecture data_mem_tb_arch of data_mem_tb is
 		i_clk        : in  std_logic;
         	i_rst        : in  std_logic;
  	       	i_write_enable   : in  std_logic;
-        	i_write_addr : in  std_logic_vector(15 downto 0);
-        	i_read_addr : in  std_logic_vector(15 downto 0);
+        	i_addr : in  std_logic_vector(15 downto 0);
         	i_write_data : in  std_logic_vector(31 downto 0);
 		o_read_data : out std_logic_vector(31 downto 0)
         );
@@ -32,8 +30,7 @@ architecture data_mem_tb_arch of data_mem_tb is
 		o_clk        : out  std_logic;
             	o_rst        : out  std_logic;
             	o_write_enable : out  std_logic;
-            	o_write_addr : out  std_logic_vector(15 downto 0);
-            	o_read_addr : out  std_logic_vector(15 downto 0);
+            	o_addr : out  std_logic_vector(15 downto 0);
             	o_write_data : out  std_logic_vector(31 downto 0);
 		i_read_data : in std_logic_vector(31 downto 0)
         );
@@ -46,8 +43,7 @@ t1: data_mem_tester port map(
 	o_clk => clk_r,
 	o_rst => rst_r,
 	o_write_enable => write_enable_r,
-	o_write_addr => write_addr_r,
-	o_read_addr => read_addr_r,
+	o_addr => addr_r,
 	o_write_data => write_data_r,
 	i_read_data => read_data_r
 );
@@ -56,8 +52,7 @@ t2: DataMemory port map(
 	i_clk => clk_r,
 	i_rst => rst_r,
 	i_write_enable => write_enable_r,
-	i_write_addr => write_addr_r,
-	i_read_addr => read_addr_r,
+	i_addr => addr_r,
 	i_write_data => write_data_r,
 	o_read_data => read_data_r
 );
--- data_mem_tester.vhd
+++ data_mem_tester.vhd
@@ -7,8 +7,7 @@ entity data_mem_tester is
             	o_clk        : out  std_logic;
             	o_rst        : out  std_logic;
             	o_write_enable : out  std_logic;
-            	o_write_addr : out  std_logic_vector(15 downto 0);
-            	o_read_addr : out  std_logic_vector(15 downto 0);
+            	o_addr : out  std_logic_vector(15 downto 0);
             	o_write_data : out  std_logic_vector(31 downto 0);
             	i_read_data : in std_logic_vector(31 downto 0)
     	);
@@ -46,24 +45,23 @@ begin
 	wait_clk(1);
 
     	o_write_enable <= '0';
-   	o_write_addr <= ADDR0;
-    	o_read_addr <= ADDR0;
+   	o_addr <= ADDR0;
     	o_write_data <= x"AAAAAAAA";
 
         o_write_enable <= '1';
 	wait_clk(1);
 
-        o_write_addr <= ADDR1;
+        o_addr <= ADDR1;
         o_write_data <= x"AAAAAAAA";
         wait_clk(1);
 
         o_write_enable <= '0';
         wait_clk(1);
 
-	o_read_addr <= ADDR1;
+	o_addr <= ADDR1;
         wait_clk(1);
 
-        o_read_addr <= ADDR2;
+        o_addr <= ADDR2;
         wait_clk(1);
 
         o_rst <= '1';
@@ -71,7 +69,7 @@ begin
         wait for 1 ns;
         o_rst <= '0';
 
-        o_read_addr <= ADDR1;
+        o_addr <= ADDR1;
         wait;
     end process;
 
