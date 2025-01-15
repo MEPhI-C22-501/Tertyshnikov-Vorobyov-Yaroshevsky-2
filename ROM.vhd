638691c4d7a19b06d89d706f0b7bf626ea8bdf34
perfecto <67798500+pErfEcto2@users.noreply.github.com>
2025-01-15 22:11:47 +0300

    Add files via upload

---
+++ ROM.vhd
@@ -0,0 +1,35 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+library work;
+use work.ROM_pkg.all;
+
+entity ROM is
+	generic (
+		file_path : string := "program.hex"
+	);
+
+    port (
+        i_clk       : in  std_logic;
+	i_rst       : in  std_logic;
+        i_read_addr : in  std_logic_vector(15 downto 0);
+        o_read_data : out std_logic_vector(31 downto 0)
+    );
+end ROM;
+
+architecture ROM_beh of ROM is
+	signal mem : inst_array := read_hex_from_file(file_path);
+	signal read_data_r : std_logic_vector(31 downto 0);
+	
+begin
+	o_read_data <= read_data_r;
+
+	process(i_clk, i_rst)
+	begin
+		if i_rst = '1' then
+			read_data_r <= (others => '0');
+		elsif rising_edge(i_clk) then
+			read_data_r <= mem(to_integer(unsigned(i_read_addr)));
+		end if;
+	end process;
+end ROM_beh;
