281a2f51142eeb7aebbf031c506a602c78905ffa
perfecto <67798500+pErfEcto2@users.noreply.github.com>
2025-01-24 20:41:22 +0300

    Delete csr_mem_pkg.vhd

--- csr_mem_pkg.vhd
+++
@@ -1,9 +0,0 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
-package csr_mem_pkg is
-    type csr_array is array (31 downto 0) of std_logic_vector(31 downto 0);
-end csr_mem_pkg;
-
-package body csr_mem_pkg is
-end csr_mem_pkg;
